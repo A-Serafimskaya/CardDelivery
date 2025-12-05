284b3d8bee2613edafc87b0403630a40ad7fd69c
Alexandra Serafimskaya <a79256103066@gmail.com>
2025-12-05 14:21:25 +0300

    renew CardDeliveryTest Class

---
+++ README.md
@@ -0,0 +1,3 @@
+# CardDelivery 
+
+[![Java CI with Gradle](https://github.com/A-Serafimskaya/CardDelivery/actions/workflows/gradle.yml/badge.svg)](https://github.com/A-Serafimskaya/CardDelivery/actions/workflows/gradle.yml)
--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -49,46 +49,6 @@ public class CardDeliveryTest {
         $(Selectors.withText("Успешно!")).should(Condition.visible, Duration.ofSeconds(15));
     }
 
-    // Задача 2
 
-//    @Test
-//    void testAutoFil() {
-//
-//        LocalDate Date = LocalDate.now().plusDays(7);
-//        DateTimeFormatter formatterDay = DateTimeFormatter.ofPattern("dd");
-//        DateTimeFormatter formatterMonth = DateTimeFormatter.ofPattern("MMMM", new Locale("ru"));
-//        DateTimeFormatter formatterYear = DateTimeFormatter.ofPattern("yyyy", new Locale("ru"));
-//
-//        String day = Date.format(formatterDay);
-//        String month = Date.format(formatterMonth);
-//        String year = Date.format(formatterYear);
-//
-//        if (month.endsWith("я")) {
-//            month = month.substring(0, month.length() - 1);
-//        }
-//
-//        if (month.endsWith("а")) {
-//            month = month.substring(0, month.length() - 1);
-//        }
-//
-//
-//        SelenideElement form = $("form");
-//        form.$("[data-test-id='city'] input").setValue("Ек");
-//        $(Selectors.withText("Екатеринбург")).click();
-//        form.$("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-//        form.$("[data-test-id='date'] input").click();
-//
-//        SelenideElement actualPeriod = $("calendar-input__calendar-wrapper");
-//        actualPeriod.find(Condition.text(month)).find(Condition.text(year)).find(Condition.text(day)).click();
-//
-//        actualPeriod.$(day).click();
-//
-//
-//        form.$("[data-test-id='name'] input").setValue("Иванов-Петров Петр");
-//        form.$("[data-test-id='phone'] input").setValue("+79251234567");
-//        form.$("[data-test-id='agreement']").click();
-//        form.$$("button").find(text("Забронировать")).click();
-//        $(Selectors.withText("Успешно!")).should(Condition.visible, Duration.ofSeconds(15));
-//    }
 
 }
